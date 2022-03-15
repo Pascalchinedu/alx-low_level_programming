fe7d0553c0b7e7142b9c5db8869be0b8462eab7d
Pascalchinedu <pascalchinedu2000@gmail.com>
2022-03-15 05:18:06 -0700

    Nested Loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - Prints _putchar
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int str[] = ('_', 'p', 'u', 't', 'c', 'h', 'a', 'r');
-	int i, size;
-	size = sizeof(str) / sizeof(int);
-	for (i = 0; i < size; i++)
-		_putchar(str[i]);
-	_putchar('\n');
-	return 0;
+        int str[] = ('_', 'p', 'u', 't', 'c', 'h', 'a', 'r');
+        int i, size;
+        size = sizeof(str) / sizeof(int);
+        for (i = 0; i < size; i++)
+                _putchar(str[i]);
+        _putchar('\n');
+        return 0;
 }
